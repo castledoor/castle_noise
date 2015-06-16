01fde87190eb221cf1184d745889e322de626798
castledoor <b.jazzy.w@gmail.com>
2015-06-16 15:31:23 -0700

    completes tools tip hovers for elite8

---
+++ source/javascripts/elite8.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('.elite8-tool-1').mouseenter(function() {
+    $('.elite8-dialog-1, .pd-tool').css("display", "block");
+  });
+  $('.elite8-tool-1').mouseleave(function() {
+    $('.elite8-dialog-1, .pd-tool').css("display", "none");
+  });
+
+  $('.elite8-tool-2').mouseenter(function() {
+    $('.elite8-dialog-2, .pd-tool').css("display", "block");
+  });
+  $('.elite8-tool-2').mouseleave(function() {
+    $('.elite8-dialog-2, .pd-tool').css("display", "none");
+  });
+
+  $('.elite8-tool-3').mouseenter(function() {
+    $('.elite8-dialog-3, .pd-tool').css("display", "block");
+  });
+  $('.elite8-tool-3').mouseleave(function() {
+    $('.elite8-dialog-3, .pd-tool').css("display", "none");
+  });
+});
