c162020e126b45976822fba5bf86d91484baae49
castledoor <b.jazzy.w@gmail.com>
2015-06-17 08:25:43 -0700

    adds carousel and overlay on footer buttons

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -3,4 +3,45 @@ $(document).ready(function() {
     $('.explore').toggleClass('open');
     $('.open_close p, .open_close img ').toggle();
   });
+
+   $('.inner').slick({
+     dots: true,
+     infinite: true
+   });
+
+   //footer
+
+var slider_active_1 = $('.slick-slider').children().last().children().first()
+var slider_active_2 = $('.slick-slider').children().last().children().first().next()
+var slider_active_3 = $('.slick-slider').children().last().children().first().next().next()
+   $(slider_active_1).click(function() {
+    $('.words-elite-footer').css("opacity", "1");
+    $('.words-zoom-footer').css("opacity", "1");
+    $('.words-peg-footer').css("opacity", "0");
+    $('.overlay-2, .overlay-3').css("display", "block");
+    $('.overlay-3').css("margin-top", "-43px");
+    $('.overlay-2').css('margin-top', "-38px");
+    $('.overlay-1').css("display", "none");
+
+   });
+
+   $(slider_active_2).click(function() {
+    $('.words-zoom-footer').css("opacity", "1");
+    $('.words-peg-footer').css("opacity", "1");
+    $('.words-elite-footer').css("opacity", "0");
+    $('.overlay-1, .overlay-3').css("display", "block");
+    $('.overlay-2').css("display", "none");
+    $('.overlay-3').css('margin-top', "-43px");
+   });
+
+   $(slider_active_3).click(function() {
+    $('.words-elite-footer').css("opacity", "1");
+    $('.words-peg-footer').css("opacity", "1");
+    $('.words-zoom-footer').css("opacity", "0");
+    $('.overlay-2, .overlay-1').css("display", "block");
+    $('.overlay-3').css("display", "none");
+    $('.overlay-2').css('margin-top', "-43px");
+   });
+
+
 });
