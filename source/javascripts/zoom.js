94363f76bb210ce5beaccdc779060f52ce29062b
castledoor <b.jazzy.w@gmail.com>
2015-06-17 04:56:22 -0700

    activates zoom family tool tip dialogs

---
+++ source/javascripts/zoom.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $('.zoom-family-tool-1').mouseenter(function() {
+    $('.zoom-family-dialog-1, .pd-tool').css("display", "block");
+  });
+  $('.zoom-family-tool-1').mouseleave(function() {
+    $('.zoom-family-dialog-1, .pd-tool').css("display", "none");
+  });
+
+  $('.zoom-family-tool-2').mouseenter(function() {
+    $('.zoom-family-dialog-2, .pd-tool').css("display", "block");
+  });
+  $('.zoom-family-tool-2').mouseleave(function() {
+    $('.zoom-family-dialog-2, .pd-tool').css("display", "none");
+  });
+
+  $('.zoom-family-tool-3').mouseenter(function() {
+    $('.zoom-family-dialog-3, .pd-tool').css("display", "block");
+  });
+  $('.zoom-family-tool-3').mouseleave(function() {
+    $('.zoom-family-dialog-3, .pd-tool').css("display", "none");
+  });
+
+  $('.zoom-family-tool-4').mouseenter(function() {
+    $('.zoom-family-dialog-4, .pd-tool').css("display", "block");
+  });
+  $('.zoom-family-tool-4').mouseleave(function() {
+    $('.zoom-family-dialog-4, .pd-tool').css("display", "none");
+  });
+});
