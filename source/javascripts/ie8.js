73dc95a656edd55bfaee8263faf67766745a7b6e
castledoor <b.jazzy.w@gmail.com>
2015-06-19 09:47:54 -0700

    adds variables for image and font paths

--- source/javascripts/ie8.js
+++ source/javascripts/ie8.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
-  $('.explore').css("display","none");
-  $('.open_close').click(function(){
-    $('.explore').slideUp('slow');
-    $('.open_close p, .open_close img ').toggle();  
-  });
-
-
-
+  $('.explore').css("display", "none");
 });
-
-
-
-
-
