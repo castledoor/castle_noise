293b542f9f7da9f14c34876a6553f8f87d8db920
castledoor <b.jazzy.w@gmail.com>
2015-07-23 15:12:42 -0700

    adds toggle for full review styles sub artists.. not done

--- source/javascripts/blocks/main.js
+++ source/javascripts/blocks/main.js
@@ -1,26 +1,21 @@
  $(document).ready(function() {
-     $('#listitems').paginate({itemsPerPage: 5});
+  $('#listitems').paginate({itemsPerPage: 5});
 
-     $('.hamburger').click(function(){
-         var side_nav = $("nav");
-         var target_margin = 0;
-         if(side_nav.hasClass("open")){
-           target_margin = -600;
-         } else {
-           target_margin = 0;
-         }
-         TweenLite.to(side_nav, 0.6, {
-                       marginRight: target_margin, 
-                       ease: Power4.easeOut,
-                       onComplete: toggleOpen,
-                       onCompleteParams: [side_nav]
-                      });
-         //$('nav').animate({marginLeft: 0}, 400);
-     });
+  $(".triangle img").click(function(){
+    $('.main').toggleClass("open");
+    $('.main').toggleClass("slideLeft");
+    $(".drawer-wrapper").css("display", "block");
+    $(".triangle img").toggle();
+  });
 
-     function toggleOpen(_target) {
-       _target.toggleClass("open");
-     }
- });
+  $(".full-review-pull").click(function() {
+    var full_review = $(this).parent().parent().parent().parent().next().children()
+    $(full_review).toggle();
+    $('.full-review-pull').toggle();
+    $('.arrow').toggle();
+  });
+});
+
+
 
 
